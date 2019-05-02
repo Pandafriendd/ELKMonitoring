e4bc36969500ce9233759dbc0974ce71dc76a6bf
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-05-02 18:59:44 -0400

    Update csv_to_yaml.py

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -3,5 +3,5 @@ import yaml
 with open('export-two-cols.csv') as f:
     d = dict(filter(None, csv.reader(f)))
 	
-with open('output2.yml', 'w') as newf:
+with open('output2.yaml', 'w') as newf:
     yaml.dump(d, newf, default_flow_style=False, default_style='"')
