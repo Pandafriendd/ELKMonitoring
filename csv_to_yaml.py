312baa3d9fe758a91f0b62e08728afa40308997e
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-05-02 18:54:23 -0400

    add script for converting csv to yaml

---
+++ csv_to_yaml.py
@@ -0,0 +1,7 @@
+import csv
+import yaml
+with open('export-two-cols.csv') as f:
+    d = dict(filter(None, csv.reader(f)))
+	
+with open('output2.yml', 'w') as newf:
+    yaml.dump(d, newf, default_flow_style=False, default_style='"')
